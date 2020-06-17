260e7a960b57eaa3ebec5d9b976e610ff5104052
Enrique Alexandre González Sequeira <enrique.alexand.gonzalez@est.fib.upc.edu>
2020-06-17 14:12:03 +0200

    Updates

---
+++ precache-manifest.41d535f6e6fd63b7229e6924dd33ffaf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a94bff1dbfa0792c2ffbafb51d82dc",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "8c423203eda3c5a2ca5a",
+    "url": "/MyPortfolio/static/css/main.47e56045.chunk.css"
+  },
+  {
+    "revision": "a9a384d9e6023f58f818",
+    "url": "/MyPortfolio/static/js/2.f4184e63.chunk.js"
+  },
+  {
+    "revision": "8c423203eda3c5a2ca5a",
+    "url": "/MyPortfolio/static/js/main.1095c0f7.chunk.js"
+  },
+  {
+    "revision": "7bb7677cc222694e8393",
+    "url": "/MyPortfolio/static/js/runtime-main.25102f67.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/MyPortfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "0cfe661e089b285e1f837bd0d6a54ea5",
+    "url": "/MyPortfolio/static/media/DragonEngineGif.0cfe661e.gif"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/MyPortfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/MyPortfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/MyPortfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "57e4155e48f3526cdd54c33907670f56",
+    "url": "/MyPortfolio/static/media/dragonengine.57e4155e.gif"
+  },
+  {
+    "revision": "ca39369879003bbad58905f35ed366dc",
+    "url": "/MyPortfolio/static/media/falloutgo.ca393698.gif"
+  },
+  {
+    "revision": "b060ef4deb31bc8fdaf8fd686da6651f",
+    "url": "/MyPortfolio/static/media/fixitstewie.b060ef4d.gif"
+  },
+  {
+    "revision": "c681f771a6165216eb6ad48318bd1871",
+    "url": "/MyPortfolio/static/media/lemmings.c681f771.png"
+  },
+  {
+    "revision": "d80a053cb21c84992e178b25d48a0f20",
+    "url": "/MyPortfolio/static/media/litleorionengine.d80a053c.gif"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MyPortfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/MyPortfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "37f2856e2de6bcca8150e4207999a867",
+    "url": "/MyPortfolio/static/media/oniongalaxy.37f2856e.jpg"
+  },
+  {
+    "revision": "2f7249fb04bbf83edbd0d5f1d1f7f8f7",
+    "url": "/MyPortfolio/static/media/pixeldungeon_coop.2f7249fb.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/MyPortfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "e143a3efbc114ac595790938405041c0",
+    "url": "/MyPortfolio/static/media/sopra-icon.e143a3ef.jpg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/MyPortfolio/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "3dd5ef2b0d1a392ea65f1b0c7018103e",
+    "url": "/MyPortfolio/static/media/twinlightsouls.3dd5ef2b.gif"
+  }
+]);
